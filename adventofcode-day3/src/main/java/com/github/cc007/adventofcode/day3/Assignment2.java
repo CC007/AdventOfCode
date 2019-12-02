fffcc6f1c54446d31d918832b173da89f47eb783
CC007 <Coolcat_the_best2@Hotmail.com>
2019-12-02 21:12:49 +0100

    Prepare for day3

---
+++ adventofcode-day3/src/main/java/com/github/cc007/adventofcode/day3/Assignment2.java
@@ -0,0 +1,13 @@
+package com.github.cc007.adventofcode.day3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class Assignment2 {
+
+    public static void main(String[] args) {
+    }
+}
